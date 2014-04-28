ffb1b0bf9a31be07ff61eb060d39bcdb8a263680
Costa Walcott <cwalcott@gmail.com>
2014-04-28 14:52:19 -0400

    WIP - subspecs

--- ZXingObjC.podspec
+++ ZXingObjC.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'ZXingObjC'
-  s.version = '3.0.0'
+  s.version = '3.0.0-alpha.1'
   s.summary = 'An Objective-C Port of the ZXing barcode framework.'
   s.homepage = 'https://github.com/TheLevelUp/ZXingObjC'
   s.author = 'ZXingObjC team'
@@ -14,4 +14,13 @@ Pod::Spec.new do |s|
 
   s.ios.frameworks = 'AVFoundation', 'CoreGraphics', 'CoreMedia', 'CoreVideo', 'ImageIO', 'QuartzCore'
   s.osx.frameworks = 'AVFoundation', 'CoreMedia', 'QuartzCore'
+
+  s.subspec 'Core' do |ss|
+    ss.source_files = 'ZXingObjC/client/*.{h,m}', 'ZXingObjC/common/**/*.{h,m}', 'ZXingObjC/core/*.{h,m}', 'ZXingObjC/multi/*.{h,m}'
+  end
+
+  s.subspec 'QRCode' do |ss|
+    ss.dependency 'ZXingObjC/Core'
+    ss.source_files = 'ZXingObjC/qrcode/**/*.{h,m}'
+  end
 end
---
+++ ZXingObjC/qrcode/ZXingObjCQRCode.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ZXINGOBJC_QRCODE_
+
+#define _ZXINGOBJC_QRCODE_
+
+#import "ZXMultiDetector.h"
+#import "ZXMultiFinderPatternFinder.h"
+#import "ZXQRCodeMultiReader.h"
+
+#import "ZXQRCodeBitMatrixParser.h"
+#import "ZXQRCodeDataBlock.h"
+#import "ZXQRCodeDataMask.h"
+#import "ZXQRCodeDecodedBitStreamParser.h"
+#import "ZXQRCodeDecoder.h"
+#import "ZXQRCodeErrorCorrectionLevel.h"
+#import "ZXQRCodeFormatInformation.h"
+#import "ZXQRCodeMode.h"
+#import "ZXQRCodeVersion.h"
+
+#import "ZXQRCodeAlignmentPattern.h"
+#import "ZXQRCodeAlignmentPatternFinder.h"
+#import "ZXQRCodeDetector.h"
+#import "ZXQRCodeFinderPattern.h"
+#import "ZXQRCodeFinderPatternFinder.h"
+#import "ZXQRCodeFinderPatternInfo.h"
+
+#import "ZXQRCodeBlockPair.h"
+#import "ZXByteMatrix.h"
+#import "ZXQRCodeEncoder.h"
+#import "ZXQRCodeMaskUtil.h"
+#import "ZXQRCodeMatrixUtil.h"
+#import "ZXQRCode.h"
+
+#import "ZXQRCodeReader.h"
+#import "ZXQRCodeWriter.h"
+
+#endif
