6750975b9d8e2e26de094eee25eb3f316cd57e19
Michael Welles <mlwelles@gmail.com>
2014-04-28 19:03:38 -0400

    Cleaned up error handling

--- ZXDecode/main.m
+++ ZXDecode/main.m
@@ -9,22 +9,28 @@
 #import <Foundation/Foundation.h>
 #import "ZXingObjC.h"
 
+int err(NSString *message) {
+    NSFileHandle *stderrHandle = [NSFileHandle fileHandleWithStandardError];
+    if (message) {
+        [stderrHandle writeData:[message dataUsingEncoding:NSUTF8StringEncoding]];
+        [stderrHandle writeData:[@"\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    [stderrHandle writeData:[@"Usage: ZXDecode [filename]\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    return 1;
+}
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        NSFileHandle *stderrHandle = [NSFileHandle fileHandleWithStandardError];
+
         NSFileHandle *stdoutHandle = [NSFileHandle fileHandleWithStandardOutput];
         if (argc != 2) {
-            [stderrHandle writeData:[@"Usage: ZXDecode [filename]\n" dataUsingEncoding:NSUTF8StringEncoding]];
-            return 1;
+            return err(nil);
         }
         NSString *fileName = [[NSString alloc] initWithBytes:argv[1] length:strlen(argv[1]) encoding:NSASCIIStringEncoding];
         
         NSImage *image = [[NSImage alloc] initWithContentsOfFile:fileName];
         if (! image) {
-            [stderrHandle writeData:[[NSString stringWithFormat:@"Failed to open image '%@'\n", fileName ] dataUsingEncoding:NSUTF8StringEncoding]];
-            [stderrHandle writeData:[@"Usage: ZXDecode [filename]\n" dataUsingEncoding:NSUTF8StringEncoding]];
-            return 1;
+            return err([NSString stringWithFormat:@"Failed to open image '%@'\n", fileName ]);
         }
         CGImageRef imageToDecode = [image CGImageForProposedRect:NULL context:[NSGraphicsContext currentContext] hints:nil];
         ZXLuminanceSource* source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
@@ -48,6 +54,7 @@ int main(int argc, const char * argv[])
             // The barcode format, such as a QR code or UPC-A
             //ZXBarcodeFormat format = result.barcodeFormat;
         } else {
+            return err([NSString stringWithFormat:@"Failed to open image '%@'\n", fileName ]);
             // Use error to determine why we didn't get a result, such as a barcode
             // not being found, an invalid checksum, or a format inconsistency.
         }
